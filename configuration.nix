eeec8413bb156af82ace990d90da556bdfe9c6a8
Samir Talwar <samir@functional.computer>
2023-03-18 15:08:21 +0100

    Limit the bootloader to 5 NixOS configurations.

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
