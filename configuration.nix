6ffd8207cfe8caed379e7be179dd4725447575ea
Samir Talwar <samir@functional.computer>
2023-10-25 16:28:39 +0200

    Attempt to fix screen sharing by fiddling with configuration.
    Doesn't work, but committing anyway.

--- configuration.nix
+++ configuration.nix
@@ -37,12 +37,15 @@
   xdg = {
     portal = {
       enable = true;
+      wlr.enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
       ];
     };
   };
+
+  # Enable dbus
+  services.dbus.enable = true;
 
   # Enable audio
   services.pipewire = {
